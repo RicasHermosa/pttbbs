d579edc12997858f494dd5a4bfcbb6e7d69ec257
my1938 <my1938.tw@gmail.com>
2018-03-18 18:56:01 +0800

    beta version problem fix

--- mbbsd/bbs.c
+++ mbbsd/bbs.c
@@ -327,7 +327,7 @@ save_violatelaw(void)
 
 static time4_t  *board_note_time = NULL;
 
-void sendAlert(FILE *fin, char *board, char *title, char *owner) {
+void keeplog(FILE *fin, char *board, char *title, char *owner) {
     fileheader_t fhdr;
     char genbuf[256], buf[512];
     FILE *fout;
@@ -357,6 +357,30 @@ void sendAlert(FILE *fin, char *board, char *title, char *owner) {
     if((bid = getbnum(board)) > 0)
 	touchbtotal(bid);
 
+}
+
+int sendAlert(int argc, char **argv)
+{
+    FILE *fp;
+
+    attach_SHM();
+    resolve_boards();
+    if(argc != 5) {
+	printf("usage: %s <board name> <title> <owner> <file>\n", argv[0]);
+	return 0;
+    }
+    
+    if(strcmp(argv[4], "-") == 0)
+	fp = stdin;
+    else {
+	fp = fopen(argv[4], "r");
+	if(!fp) {
+	    perror(argv[4]);
+	    return 1;
+	}
+    }
+    keeplog(fp, argv[1], argv[2], argv[3]);
+    return 0;
 }
 
 void
